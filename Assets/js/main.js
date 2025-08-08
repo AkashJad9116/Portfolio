b6fe7f0b87348c74886fc532b6c9450fa684452d
AkashJad9116 <akashjad9116@gmail.com>
2025-08-08 15:10:48 -0400

    removing jekyll... trying out layout from scratch

---
+++ Assets/js/main.js
@@ -0,0 +1,19 @@
+// assets/js/main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Typed("#typed-role", {
+    strings: [
+      "Data Engineer",
+      "Machine Learning Developer",
+      "Full Stack Engineer",
+      "AI Enthusiast"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    smartBackspace: true
+  });
+});
